c19f30b1798749f984d16d04cbea27edcd47f950
David Albert <davidbalbert@gmail.com>
2012-02-18 16:54:16 -0500

    use DEFINE_BINARY_OPERATION to dry up code

--- number.c
+++ number.c
@@ -2,59 +2,23 @@
 
 #include "calc.h"
 
-struct number ADD(struct number x, struct number y) {
-        if (IS_RATIONAL(x) || IS_RATIONAL(y)) {
-                ENSURE_RATIONAL(x);
-                ENSURE_RATIONAL(y);
-                return ADD_RATIONAL(x, y);
+#define DEFINE_BINARY_OPERATION(name) \
+        struct number name(struct number x, struct number y) { \
+                if (IS_RATIONAL(x) || IS_RATIONAL(y)) { \
+                        ENSURE_RATIONAL(x); \
+                        ENSURE_RATIONAL(y); \
+                        return name##_RATIONAL(x, y); \
+                } \
+                else { \
+                        return name##_INTEGER(x, y); \
+                } \
         }
-        else {
-                return ADD_INTEGER(x, y);
-        }
-}
 
-struct number SUBTRACT(struct number x, struct number y) {
-        if (IS_RATIONAL(x) || IS_RATIONAL(y)) {
-                ENSURE_RATIONAL(x);
-                ENSURE_RATIONAL(y);
-                return SUBTRACT_RATIONAL(x, y);
-        }
-        else {
-                return SUBTRACT_INTEGER(x, y);
-        }
-}
-
-struct number MULTIPLY(struct number x, struct number y) {
-        if (IS_RATIONAL(x) || IS_RATIONAL(y)) {
-                ENSURE_RATIONAL(x);
-                ENSURE_RATIONAL(y);
-                return MULTIPLY_RATIONAL(x, y);
-        }
-        else {
-                return MULTIPLY_INTEGER(x, y);
-        }
-}
-struct number DIVIDE(struct number x, struct number y) {
-        if (IS_RATIONAL(x) || IS_RATIONAL(y)) {
-                ENSURE_RATIONAL(x);
-                ENSURE_RATIONAL(y);
-                return DIVIDE_RATIONAL(x, y);
-        }
-        else {
-                return DIVIDE_INTEGER(x, y);
-        }
-}
-
-struct number POW(struct number x, struct number y) {
-        if (IS_RATIONAL(x) || IS_RATIONAL(y)) {
-                ENSURE_RATIONAL(x);
-                ENSURE_RATIONAL(y);
-                return POW_RATIONAL(x, y);
-        }
-        else {
-                return POW_INTEGER(x, y);
-        }
-}
+DEFINE_BINARY_OPERATION(ADD);
+DEFINE_BINARY_OPERATION(SUBTRACT);
+DEFINE_BINARY_OPERATION(MULTIPLY);
+DEFINE_BINARY_OPERATION(DIVIDE);
+DEFINE_BINARY_OPERATION(POW);
 
 struct number NEGATE(struct number x) {
         if (IS_RATIONAL(x))
